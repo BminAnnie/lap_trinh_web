820d7ad6f34d7b9369b63b00548502ea40d34397
Pill <tranvinhphuc251213@gmail.com>
2022-12-01 22:55:25 +0700

    update simple model CRUD carts, orders

--- BackEnd/services/cart/crud_cart.php
+++ BackEnd/services/cart/crud_cart.php
@@ -1,60 +1,82 @@
-<!-- METHOD TO GET CART ITEM BEFORE RENDER -->
-
-<!--
- -->
-
-<!-- Lưu ý: cột out['role'] để phân biệt user hay admin mà render -->
 <?php
-include("../config/db_connect.php");
+session_start();
+include("../../config/db_connect.php");
 
-if (isset($_POST['action'])) {
-    // ADD DOG TO CART
-    if ($_GET['action'] == 'add_to_cart') {
-        include("../config/db_connect.php");
-        $id = $_GET['id']; // id of dog
-        $quantity = $_POST['quantity'];
-        $sql = "SELECT * FROM dog WHERE id=$id";
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_array($result);
-        if ($row) {
-            $new_product = array(array('id' => $row['id'], 'name' => $row['name'], 'quantity' => $quantity, 'price' => $row['price'], 'image' => $row['image']));
+switch ($method) {
+    case 'GET':
+        if (isset($_GET['action'])) {
+            // GET ALL PRODUCT IN CART
+            if ($_GET['action'] == 'get_all') {
+                foreach ($_SESSION['cart'] as $cart_item) {
+                    $product[] = array('name' => $cart_item['name'], 'id' => $cart_item['id'], 'quantity' => $quantity, 'price' => $cart_item['price'], 'image' => $cart_item['image']);
+                }
+            }
+            echo json_encode($product);
+            mysqli_close($conn);
+        }
+        break;
+    case 'POST':
+        if (isset($_POST['action'])) {
+            // ADD PRODUCT TO CART
+            if ($_GET['action'] == 'add') {
+                $id = $_POST['id']; // id of dog
+                $quantity = $_POST['quantity'];
+                $sql = "SELECT * FROM 'product' WHERE id=$id";
+                $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_array($result);
+                if ($row) {
+                    $new_product = array(array('id' => $row['id'], 'name' => $row['name'], 'quantity' => $quantity, 'price' => $row['price'], 'image' => $row['image']));
 
-            if (isset($_SESSION['cart'])) {
-                $found = false;
+                    if (isset($_SESSION['cart'])) {
+                        $found = false;
+                        foreach ($_SESSION['cart'] as $cart_item) {
+                            if ($cart_item['id'] == $id) {
+                                $product[] = array('name' => $cart_item['name'], 'id' => $cart_item['id'], 'quantity' => $cart_item['quantity'] + 1, 'price' => $cart_item['price'], 'image' => $cart_item['image']);
+                                $found = true;
+                            } else {
+                                $product[] = array('name' => $cart_item['name'], 'id' => $cart_item['id'], 'quantity' => $quantity, 'price' => $cart_item['price'], 'image' => $cart_item['image']);
+                            }
+                        }
+                        if ($found == false) {
+                            $_SESSION['cart'] = array_merge($product, $new_product);
+                        } else {
+                            $_SESSION['cart'] = $new_product;
+                        }
+                    } else {
+                        $_SESSION['cart'] = $new_product;
+                    }
+                }
+                echo json_encode(array("status" => 200));
+                mysqli_close($conn);
+            }
+            // EDIT CART ITEM
+            if ($_POST['action'] == 'edit') {
+                $id = $_POST['id'];
+                $quantity = $_POST['quantity'];
                 foreach ($_SESSION['cart'] as $cart_item) {
                     if ($cart_item['id'] == $id) {
-                        $product[] = array('name' => $cart_item['name'], 'id' => $cart_item['id'], 'quantity' => $cart_item['quantity'] + 1, 'price' => $cart_item['price'], 'image' => $cart_item['image']);
-                        $found = true;
-                    } else {
                         $product[] = array('name' => $cart_item['name'], 'id' => $cart_item['id'], 'quantity' => $quantity, 'price' => $cart_item['price'], 'image' => $cart_item['image']);
                     }
                 }
-                if ($found == false) {
-                    $_SESSION['cart'] = array_merge($product, $new_product);
-                } else {
-                    $_SESSION['cart'] = $new_product;
+                $_SESSION['card'] = $product;
+                echo json_encode(array("status" => 200));
+            }
+            // DELETE CART ITEM
+            if ($_POST['action'] == 'delete') {
+                $id = $_POST['id'];
+                foreach ($_SESSION['cart'] as $cart_item) {
+                    if ($cart_item['id'] != $id) {
+                        $product[] = array('name' => $cart_item['name'], 'id' => $cart_item['id'], 'quantity' => $cart_item['quantity'], 'price' => $cart_item['price'], 'image' => $cart_item['image']);
+                    }
                 }
-            } else {
-                $_SESSION['cart'] = $new_product;
+                $_SESSION['card'] = $product;
+                echo json_encode(array("status" => 200));
+            }
+            // DELETE ALL PRODUCT IN CART
+            if ($_POST['action'] == 'delete_all') {
+                unset($_SESSION['cart']);
+                echo json_encode(array("status" => 200));
             }
         }
-        // header("Location: gio_hang.php");
-        mysqli_close($conn);
-    }
-    // DELETE ALL DOG IN CART
-    if ($_GET['action'] == 'delete_cart') {
-        unset($_SESSION['cart']);
-        // header("Location: gio_hang.php");
-    }
-    // DELETE CART ITEM
-    if ($_GET['action'] == 'delete_cart_item') {
-        $id = $_GET['delete_order'];
-        foreach ($_SESSION['cart'] as $cart_item) {
-            if ($cart_item['id'] != $id) {
-                $product[] = array('name' => $cart_item['name'], 'id' => $cart_item['id'], 'quantity' => $cart_item['quantity'], 'price' => $cart_item['price'], 'image' => $cart_item['image']);
-            }
-        }
-        $_SESSION['card'] = $product;
-        // header("Location: gio_hang.php");
-    }
+        break;
 }
--- BackEnd/services/orders/crud_orders.php
+++ BackEnd/services/orders/crud_orders.php
@@ -1,3 +1,77 @@
+
 <?php
-session_start();
-include("../config/db_connect.php");
+include("../../config/db_connect.php");
+
+switch ($method) {
+    case 'GET':
+        if (isset($_GET['action'])) {
+            // GET ALL ORDERS
+            if ($_GET['action'] == 'get_all') {
+                $result = mysqli_query($conn, "SELECT * FROM orders");
+            }
+            $result = mysqli_fetch_all($result);
+            mysqli_close($conn);
+            echo json_encode($result);
+        }
+        break;
+    case 'POST':
+        if (isset($_POST['action'])) {
+            // ADD ORDERS
+            if ($_POST['action'] == 'add') {
+                if (!isset($_POST['username']) || !isset($_POST['phone']) || !isset($_POST['address']) || !isset($_POST['content'])) {
+                    echo json_encode("Wrong order");
+                    return;
+                }
+
+                $username = $_POST['username'];
+                $phone = $_POST['phone'];
+                $address = $_POST['address'];
+                $productName = $_POST['productName'];
+                $image = $_POST['image'];
+                $quantity = $_POST['quantity'];
+                $comment = $_POST['comment'];
+
+                $query = "INSERT INTO orders(username, phone, address, productName, image, quantity, comment) VALUES('$username','$phone','$address','$productName','$image','$quantity', '$comment')";
+                $result = mysqli_query($conn, $query);
+                if ($result) {
+                    echo json_encode(array("status" => 200));
+                } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                }
+
+                mysqli_close($conn);
+            }
+            // EDIT ORDERS
+            if ($_POST['action'] == 'edit') {
+                if (!isset($_POST['id']) || !isset($_POST['status'])) {
+                    echo json_encode("Wrong edit order");
+                    return;
+                }
+
+                $id = $_POST['id'];
+                $status = $_POST['status'];
+
+                $query = "UPDATE orders SET status='$status' WHERE id='$id'";
+                $result = mysqli_query($conn, $query);
+                if ($result) {
+                    echo json_encode(array("status" => 200));
+                } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                }
+                mysqli_close($conn);
+            }
+            // DELETE ORDERS
+            if ($_POST['action'] == 'delete') {
+                $id = $_POST['id'];
+                $sql = "DELETE FROM `orders` WHERE id=$id";
+                $result = mysqli_query($conn, $sql);
+
+                if ($result) {
+                    echo json_encode(array("status" => 200));
+                } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                }
+            }
+        }
+        break;
+}
