704b7f281eead5667cbd0dc2db2d05d5d0c7fc5a
Bao Nguyen <nguyenvanbaonguyennth@gmail.com>
2022-12-12 18:05:48 +0700

    final

--- FrontEnd/components/PetKnowledge/KnowledgeCard.js
+++ FrontEnd/components/PetKnowledge/KnowledgeCard.js
@@ -3,10 +3,11 @@ import React from 'react';
 const KnowledgeCard = ({
     contentUp = ' What is a Pomeranian? How to Identify Pomeranian Dogs',
     contentDown = '  The Pomeranian, also known as the Pomeranian (Pom dog), is always in the top of the cutest pets. Not only that, the small, lovely, smart, friendly, and skillful circus dog breed.',
+    src = "images/knowledge/image1.jpg",
 }) => {
     return (
         <div className="w-full  rounded-[12px] shadowCard bg-[#ffffff] p-2 flex flex-col">
-            <img src="images/knowledge/image1.png" alt="" className="w-full " />
+            <img src={src} alt="" className="w-full " />
             <div className="knowledge__content p-2 overflow-clip">
                 <div className="h-[20px] px-[10px] py-[2px] bg-[#00A7E7] rounded-[28px] inline-block text-center text-[10px] leading-4 text-[#FDFDFD]">
                     Pet knowledge
--- FrontEnd/components/PetKnowledge/index.js
+++ FrontEnd/components/PetKnowledge/index.js
@@ -13,9 +13,17 @@ const PetKnowledge = () => {
                 isHideButton={true}
             />
             <div className="grid laptop:grid-cols-3 laptop:gap-x-[20px] gap-y-5 ">
-                <KnowledgeCard />
-                <KnowledgeCard />
-                <KnowledgeCard />
+                <KnowledgeCard contentUp='Why is my doggie peeing everywhere?'
+                contentDown='Doggies urinating uncontrollably can be the sign of diseases. It may be a urinary tract infection or gastrointestinal problem, or even diabetes. Bring your doggie to the vet for a check-up.'
+                src='/images/knowledge/pee.png'
+                />
+                <KnowledgeCard contentUp='What is your dog thinking?'
+                contentDown='You can read form their eyes, ears or tails. If they look straight means they are confident, if their mount are painting, that means they are very excited.'
+                src='/images/knowledge/read.png'/>
+                <KnowledgeCard contentUp='Cat paw care tips'
+                contentDown='Moisturizing
+                If your kitty’s paw pads become irritated or cracked, help your cats drink more and also try moisturizing them with olive, coconut or another food-quality oil that will be safe for them to lick.'
+                src='/images/knowledge/care.png'/>
             </div>
         </div>
     );
